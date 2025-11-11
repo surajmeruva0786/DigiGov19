b32103b9372fe86f5c837eff0697c42b1d47361a
surajmeruva0786 <meruva24102@iiitnr.edu.in>
2025-11-11 20:13:28 +0530

    update console logs

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  import "./firebase"; // ensure Firebase initializes and logs in console
 
   createRoot(document.getElementById("root")!).render(<App />);
   
